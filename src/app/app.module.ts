3f811ae4027b532f21ef2fe8d92f1c64a0588e54
Paul Allain <paul.allain17@gmail.com>
2019-02-23 22:24:58 +0000

    New currency component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CurrencyComponent } from './currency/currency.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule
