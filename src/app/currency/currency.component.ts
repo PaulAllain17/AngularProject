2f77e630932576715e909e32fe9aff2db341db11
Paul Allain <paul.allain17@gmail.com>
2019-02-24 12:09:44 +0000

    New DataService that retieves currency rates from third party.

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-currency',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyComponent implements OnInit {
 
-  constructor() { }
+  currencyRates: any;
+  rateList: any;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getCurrencyRates().subscribe(rates => {
+      this.currencyRates = rates;
+      console.log(this.currencyRates);
+      this.rateList = Object.keys(this.currencyRates.rates)
+        .map(key => ({type: key, value: this.currencyRates.rates[key]}))
+        .sort((a, b) => a.type.localeCompare(b.type));
+      console.log(this.rateList);
+    });
   }
+
+  // myClick(){
+  //   this.data.getCurrencyRates().subscribe(rates => {
+  //     this.currencyRates = rates;
+  //     console.log(this.currencyRates);
+  //   });
+  // }
 
 }
