cb1eac18750faf68675532493b763da61a076cd8
Paul Allain <paul.allain17@gmail.com>
2019-02-26 00:38:53 +0000

    Fix failing tests

--- src/app/currency/currency.component.spec.ts
+++ src/app/currency/currency.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CurrencyComponent } from './currency.component';
 
 describe('CurrencyComponent', () => {
@@ -8,6 +8,7 @@ describe('CurrencyComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
       declarations: [ CurrencyComponent ]
     })
     .compileComponents();
